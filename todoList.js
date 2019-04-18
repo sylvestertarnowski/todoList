338ea493a1cec753ba91e77669f9f2bfa4884e07
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-04-18 05:48:58 +0200

    Added todos now display the 'delete button' animation correctly

--- todoList.js
+++ todoList.js
@@ -20,7 +20,7 @@ $("input[type='text']").keypress(function(event) {
   if(event.which === 13) {
     let todoText = $(this).val();
     $(this).val("");
-    $("ul").append("<li><i class='fas fa-trash-alt'></i>" + todoText + "</li>");
+    $("ul").append("<li><span><i class='fas fa-trash-alt'></span></i>" + todoText + "</li>");
   }
 });
 
